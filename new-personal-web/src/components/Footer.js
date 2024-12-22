a0a21ba1851cf7c2278d8049346eeae15ecea65f
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-12-21 23:16:56 -0500

    more changes yay

--- new-personal-web/src/components/Footer.js
+++ new-personal-web/src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
     <footer className="footer">
-        <p>&nbsp {new Date().getFullYear()} Antonio's Website. All rights reserved</p>
+        <p> &copy; {new Date().getFullYear()} Antonio's Website. All rights reserved</p>
     </footer>
 );
 
