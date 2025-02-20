cee9bbf31826394a7427058f08f9d8c498f9caff
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2025-02-20 11:39:06 -0500

    Update Home.js

--- new-personal-web/src/components/Home.js
+++ new-personal-web/src/components/Home.js
@@ -4,8 +4,11 @@ import "./Home.css";
 import headshot from './assets/profile-pic.jpg';
 import linkedin from './assets/linkedin.png';
 import github from './assets/github.png';
+import resume from './assets/CSResume2025.pdf';
 
-const resume = "../../public/CSResume2025.pdf";
+const openResume = () => {
+    window.open(resume, "_blank");
+};
 
 const Home = () => (
 
@@ -19,9 +22,7 @@ const Home = () => (
                 <h1 className="my-name">Antonio Mendoza</h1>
                 <h3>Computer Science Student at University of Georgia</h3>
                 <div className="btns">
-                    <a href={resume} target="_blank" rel="noopener noreferrer">
-                        <button className="link-btn">View CV</button>
-                    </a>
+                    <button className="link-btn" onClick={openResume}>View CV</button>
                     <button className="link-btn"><Link to="/contact">Contact Me</Link></button>
                 </div>
                 <div>
