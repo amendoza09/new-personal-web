0790110a5fab67039b6cacb3c603d93408fb317f
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2025-02-20 11:16:15 -0500

    resume button fix

--- new-personal-web/src/components/Home.js
+++ new-personal-web/src/components/Home.js
@@ -4,7 +4,8 @@ import "./Home.css";
 import headshot from './assets/profile-pic.jpg';
 import linkedin from './assets/linkedin.png';
 import github from './assets/github.png';
-import resume from './assets/CSResume2025.pdf';
+
+const resume = process.env.PUBLIC_URL + "/CSResume2025.pdf";
 
 const Home = () => (
 
@@ -18,7 +19,9 @@ const Home = () => (
                 <h1 className="my-name">Antonio Mendoza</h1>
                 <h3>Computer Science Student at University of Georgia</h3>
                 <div className="btns">
-                    <a href={resume} target="_blank"><button className="link-btn">View CV</button></a>
+                    <a href={resume} target="_blank" rel="noopener noreferrer">
+                        <button className="link-btn">View CV</button>
+                    </a>
                     <button className="link-btn"><Link to="/contact">Contact Me</Link></button>
                 </div>
                 <div>
