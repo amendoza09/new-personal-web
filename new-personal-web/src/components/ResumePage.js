34a27176786f86127717ac9c5aad280fded23656
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2025-02-20 15:14:13 -0500

    Update ResumePage.js

--- new-personal-web/src/components/ResumePage.js
+++ new-personal-web/src/components/ResumePage.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.css";
-import Resume from "./assets/CSResume2025.pdf";
 
 const ResumePage = () => (
     <section id="resume-page" className="home transition-fade">
-        <embed src={Resume} width="100%" height ="700px"/>
+        <embed src={`${process.env.PUBLIC_URL}/CSResume2025.pdf`} width="100%" height ="700px" type="application/pdf"/>
     </section>
 );
 
