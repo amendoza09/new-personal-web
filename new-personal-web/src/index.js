38ebaec26be4fa6e87bc72c5aa74a17767e83c55
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2025-02-20 15:32:05 -0500

    Update index.js

--- new-personal-web/src/index.js
+++ new-personal-web/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <BrowserRouter basename="/new-personal-web">
+ReactDOM.render(
+    <Router basename="/new-personal-web">
       <App />
-    </BrowserRouter>
+    </Router>,
+    document.getElementById("root")
 );
